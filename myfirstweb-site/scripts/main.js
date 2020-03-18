28df433345f64904a55583500c227468a30b44f1
Vaczzy <49486975+Vaczzy@users.noreply.github.com>
2020-03-18 19:19:59 +0800

    Update main.js

--- myfirstweb-site/scripts/main.js
+++ myfirstweb-site/scripts/main.js
@@ -8,8 +8,8 @@ myImage.onclick = function() {
       myImage.setAttribute('src', 'images/CSUlogo.jpg');
     }
 }
-//
-let myButton = document.querySelector('button');
+//网页开始时 或点击按钮时
+let myButton1 = document.querySelector('#username');
 let myHeading = document.querySelector('#welcome');
 function setUserName() {
     let myName = prompt('请输入你的名字。');
@@ -21,12 +21,53 @@ function setUserName() {
       myHeading.innerHTML = '欢迎您，' + myName;
     }
 }
+//初始化
 if(!localStorage.getItem('name')) {
     setUserName();
 } else {
     let storedName = localStorage.getItem('name');
     myHeading.textContent = '欢迎您，' + storedName;
 }
-myButton.onclick = function() {
+myButton1.onclick = function() {
     setUserName();
+}
+
+
+//测试
+let myButton2 = document.querySelector('#getloc');
+let myLoction=document.querySelector('#location');
+var options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+};
+function success(pos) {
+    var crd = pos.coords;
+    var loctext
+    //console.log('Your current position is:');
+    if(crd.latitude>0){
+        loctext=crd.latitude+'N,';
+    }
+    else{
+        loctext=crd.latitude.substring(1)+'S,';
+    }
+    if(crd.longitude>0){
+        loctext+=crd.longitude+'E,';
+    }
+    else{
+        var tmp=crd.longitude;
+        loctext+=tmp.substring(1)+'W,';
+    }
+    //console.log('Latitude : ' + crd.latitude);
+    //console.log('Longitude: ' + crd.longitude);
+    loctext+="miss:"+crd.accuracy+".";
+    //console.log('More or less ' + crd.accuracy + ' meters.');
+    //return loctext;
+    myLoction.textContent = loctext;
+};
+function error(err) {
+    console.warn('ERROR(' + err.code + '): ' + err.message);
+};
+myButton2.onclick = function() {
+    navigator.geolocation.getCurrentPosition(success, error, options);
 }
